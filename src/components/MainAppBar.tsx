8762b70b85533da80434b3f1867d0381ae4c2d9e
carlos <cfebres@fullstacklabs.co>
2021-05-18 17:47:59 -0400

    wip

--- src/components/MainAppBar.tsx
+++ src/components/MainAppBar.tsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Avatar,
+  Grid,
+} from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { DRAWER_WIDTH } from "./MainLayout";
 import MenuIcon from "@material-ui/icons/Menu";
+import AvatarImg from "../assets/images/avatar.png";
+import SearchIcon from "@material-ui/icons/Search";
+import { COLORS } from "../utils/colors";
 
 interface MainAppBarProps {
-  onDrawerToggle: () => void;
+  onToggle: () => void;
 }
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     appBar: {
+      boxShadow: "none",
+      paddingLeft: 40,
+      paddingRight: 40,
       [theme.breakpoints.up("sm")]: {
         width: `calc(100% - ${DRAWER_WIDTH}px)`,
         marginLeft: DRAWER_WIDTH,
@@ -22,26 +35,51 @@ const useStyles = makeStyles((theme: Theme) =>
         display: "none",
       },
     },
+    grow: {
+      flexGrow: 1,
+    },
+    profileContainer: {
+      width: "auto",
+      marginLeft: 8,
+    },
+    toolbar: {
+      minHeight: 90,
+    },
+    searchIcon: {
+      color: COLORS.blueGray["500"],
+    },
   })
 );
 
-export const MainAppBar: React.FC<MainAppBarProps> = ({ onDrawerToggle }) => {
+export const MainAppBar: React.FC<MainAppBarProps> = ({ onToggle }) => {
   const classes = useStyles();
   return (
-    <AppBar position="fixed" className={classes.appBar}>
-      <Toolbar>
+    <AppBar position="fixed" color="transparent" className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
         <IconButton
           color="inherit"
           aria-label="open drawer"
           edge="start"
-          onClick={onDrawerToggle}
+          onClick={onToggle}
           className={classes.menuButton}
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap>
-          Responsive drawer
-        </Typography>
+        <SearchIcon className={classes.searchIcon} />
+        <div className={classes.grow} />
+        <Avatar src={AvatarImg} alt="avatar" />
+        <Grid container direction="column" className={classes.profileContainer}>
+          <Grid item>
+            <Typography variant="body1" color="textSecondary" noWrap>
+              Colin Spence
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1" noWrap>
+              0x7301cf-0eb0aa6
+            </Typography>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
---
+++ src/components/NavPanel.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { Hidden, Drawer, List } from "@material-ui/core";
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  useTheme,
+} from "@material-ui/core/styles";
+import ProjectIcon from "../assets/images/project.svg";
+import DorgLogoIcon from "../assets/images/dorg-logo.svg";
+import { DRAWER_WIDTH } from "./MainLayout";
+import { MenuItem } from "./List/MenuItem";
+
+interface NavPanelProps {
+  open: boolean;
+  onToggle: () => void;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: DRAWER_WIDTH,
+        flexShrink: 0,
+      },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: DRAWER_WIDTH,
+    },
+    logoContainer: {
+      paddingLeft: 16,
+      paddingTop: 24,
+    },
+    list: {
+      marginTop: 32,
+    },
+    listItem: {
+      padding: 16,
+    },
+  })
+);
+
+export const NavPanel: React.FC<NavPanelProps> = ({ open, onToggle }) => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const drawer = (
+    <div>
+      <div className={classes.logoContainer}>
+        <img src={DorgLogoIcon} alt="Project Icon" />
+      </div>
+      <List className={classes.list}>
+        <MenuItem
+          path="/"
+          text="Projects"
+          icon={<img src={ProjectIcon} alt="Project Icon" />}
+        />
+      </List>
+    </div>
+  );
+
+  return (
+    <nav className={classes.drawer}>
+      <Hidden smUp implementation="css">
+        <Drawer
+          variant="temporary"
+          anchor={theme.direction === "rtl" ? "right" : "left"}
+          open={open}
+          onClose={onToggle}
+          classes={{ paper: classes.drawerPaper }}
+          ModalProps={{ keepMounted: true }}
+        >
+          {drawer}
+        </Drawer>
+      </Hidden>
+      <Hidden xsDown implementation="css">
+        <Drawer
+          classes={{ paper: classes.drawerPaper }}
+          variant="permanent"
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Hidden>
+    </nav>
+  );
+};
