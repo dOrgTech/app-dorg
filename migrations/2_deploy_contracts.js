1911ca3a9527aaf909cfabdf35d5c9c1d816a38b
JT <jason.s.theobald@gmail.com>
2021-05-21 00:25:18 -0700

    Update deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,4 @@ const dOrgProjectFactory = artifacts.require("dOrgProjectFactory");
 
 module.exports = function (deployer) {
   deployer.deploy(dOrgProjectFactory)
-    .then(function(c){
-      return c.createProject(
-      "0x28fd4cD79fa36cD71B37CFbCe592aB86Bf2701dD",
-      "0x6Cea126D014D0D7D1A4cB9D72e929BDea39dB9aE"
-    )});
 };
