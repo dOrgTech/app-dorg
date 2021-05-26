4cb53d63c06a2f372594ecdb23b00cb6d355a186
JT <jason.s.theobald@gmail.com>
2021-05-25 20:19:20 -0700

    Handle Submit creates contract.

--- src/components/CreateProjectForm.js
+++ src/components/CreateProjectForm.js
@@ -4,11 +4,18 @@ import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import { ActiveButton } from "./button/ActiveButton";
 import Web3 from "web3";
+import { ContactlessOutlined } from "@material-ui/icons";
 
 let web3 = new Web3("ws://localhost:8545");
-let abi = [{"inputs":[], "stateMutability":"nonpayable", "type":"constructor"}, {"anonymous":false, "inputs":[{"indexed":false, "internalType":"address", "name":"cloneAddress", "type":"address"}], "name":"CloneAddress", "type":"event"}, {"anonymous":false, "inputs":[{"indexed":false, "internalType":"address", "name":"logicAddress", "type":"address"}], "name":"LogicAddress", "type":"event"}, {"inputs":[{"internalType":"string", "name":"_projectName", "type":"string"}, {"internalType":"address", "name":"_treasuryWallet", "type":"address"}, {"internalType":"address", "name":"_finderWallet", "type":"address"}, {"internalType":"address", "name":"_projectWallet", "type":"address"}], "name":"createProject", "outputs":[{"internalType":"address", "name":"", "type":"address"}], "stateMutability":"nonpayable", "type":"function"}];
+let cloneABI = [ { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "address", "name": "account", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "shares", "type": "uint256" } ], "name": "PayeeAdded", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "address", "name": "from", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "PaymentReceived", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "PaymentReleased", "type": "event" }, { "inputs": [ { "internalType": "uint256", "name": "index", "type": "uint256" } ], "name": "payee", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address payable", "name": "account", "type": "address" } ], "name": "release", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "account", "type": "address" } ], "name": "released", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "account", "type": "address" } ], "name": "shares", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalReleased", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalShares", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "stateMutability": "payable", "type": "receive" }, { "inputs": [], "name": "getName", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "_projectName", "type": "string" }, { "internalType": "address[]", "name": "_payees", "type": "address[]" }, { "internalType": "uint256[]", "name": "_shares", "type": "uint256[]" } ], "name": "initialize", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [ { "internalType": "address[]", "name": "payees", "type": "address[]" }, { "internalType": "uint256[]", "name": "shares_", "type": "uint256[]" } ], "name": "initialize", "outputs": [], "stateMutability": "payable", "type": "function" } ];
+let factoryABI = [ { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "address", "name": "cloneAddress", "type": "address" } ], "name": "CloneAddress", "type": "event" }, { "inputs": [], "name": "logicImplementation", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "_projectName", "type": "string" }, { "internalType": "address", "name": "_treasuryWallet", "type": "address" }, { "internalType": "address", "name": "_finderWallet", "type": "address" }, { "internalType": "address", "name": "_projectWallet", "type": "address" } ], "name": "createProject", "outputs": [], "stateMutability": "nonpayable", "type": "function" } ];
 let contractAddress = "0x97583cC9FC64839424ac7b089CBC678446f606Ef";
-let contract = new web3.eth.Contract(abi,  contractAddress);
+let factoryContract = new web3.eth.Contract(factoryABI,  contractAddress);
+const expectedBlockTime = 1; 
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 
 const defaultValues = {
       projectName: "",
@@ -16,6 +23,8 @@ const defaultValues = {
       finderWallet: "",
       projectWallet: "",
 };
+
+var cloneAddress;
 
 export const CreateProjectForm = () => {
 
@@ -32,9 +41,23 @@ export const CreateProjectForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     web3.eth.getAccounts().then(a => {
-      contract.methods.createProject('test',a[1],a[2],a[3])
+      factoryContract.methods.createProject('test',a[1],a[2],a[3])
         .send({from:a[4],gas:1000000})
-        .then(console.log)
+        .then(async (res) => {
+          console.log("Submitted transaction with hash: ", res.transactionHash)
+          let transactionReceipt = null
+          while (transactionReceipt == null) { // Waiting expectedBlockTime until the transaction is mined
+              transactionReceipt = await web3.eth.getTransactionReceipt(res.transactionHash);
+              await sleep(expectedBlockTime)
+          }
+          console.log("Got the transaction receipt: ", transactionReceipt)
+          cloneAddress = transactionReceipt.logs[0].address
+          let clone = new web3.eth.Contract(cloneABI,cloneAddress)
+          console.log(clone)
+          let name = await clone.methods.projectName().call();
+          console.log(name)
+        })
+
     })
   };
 
