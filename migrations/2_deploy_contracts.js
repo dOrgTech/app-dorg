b99291afc729da999e35d969f061ed88b8865f08
JT <jason.s.theobald@gmail.com>
2021-05-28 00:57:52 -0700

    Fix typo in test.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,8 @@ const dOrgProjectFactory = artifacts.require("dOrgProjectFactory");
 module.exports = function (deployer) {
   let treasuryWallet = "0x15344EcDc2c4EDFCB092E284d93c20F0529FD8a6"
   // Development
-  // let gnosisLogic = "0xb4A7C7da1631CF60A2Cf23ABc86986f99a1A7f70"
+  let gnosisLogic = "0xb4A7C7da1631CF60A2Cf23ABc86986f99a1A7f70"
   // Rinkeby
-  let gnosisLogic = "0x6851D6fDFAfD08c0295C392436245E5bc78B0185"
+  // let gnosisLogic = "0x6851D6fDFAfD08c0295C392436245E5bc78B0185"
   deployer.deploy(dOrgProjectFactory, treasuryWallet, gnosisLogic)
 };
