6a4b66e39bb937bafa273f58acc272fe37779b21
JT <jason.s.theobald@gmail.com>
2021-05-31 14:30:22 -0700

    Interface added. 8/8 tests passing on rinkeby.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,22 +1,9 @@
 const dOrgProjectFactory = artifacts.require("dOrgProjectFactory");
 
 module.exports = function (deployer) {
-
-  let env = process.env.NODE_ENV || "development";
-
-  console.log("Environment: " + env);
-  
-  let gnosisLogicAddress;
-
-  if(env ==='development'){
-    // Put the address of your locally deployed GnosisSafe.sol here.
-    gnosisLogicAddress = "0xb4A7C7da1631CF60A2Cf23ABc86986f99a1A7f70" 
-  }
-
-  if(env ==='rinkeby'){
-    // Rinkeby address of GnosisSafe.sol.
-    gnosisLogicAddress = "0xb4A7C7da1631CF60A2Cf23ABc86986f99a1A7f70" 
-  }
-
-  deployer.deploy(dOrgProjectFactory, gnosisLogicAddress)
+  // Development
+  // let gnosisLogic = "0xb4A7C7da1631CF60A2Cf23ABc86986f99a1A7f70"
+  // Rinkeby
+  let gnosisLogic = "0x6851D6fDFAfD08c0295C392436245E5bc78B0185"
+  deployer.deploy(dOrgProjectFactory, gnosisLogic)
 };
