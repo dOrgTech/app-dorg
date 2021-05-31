32c25857f69eb6f7e1506fe12581c0329f03dc66
JT <jason.s.theobald@gmail.com>
2021-05-31 09:26:28 -0700

    Run prettier

--- contracts/dOrgProjectFactory.sol
+++ contracts/dOrgProjectFactory.sol
@@ -6,11 +6,10 @@ import "./dOrgProject.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
 /**
-* @dev Creates an instance of `dOrgProjectFactory`. 
-*/
+ * @dev Creates an instance of `dOrgProjectFactory`.
+ */
 
 contract dOrgProjectFactory {
-
     address public immutable treasuryWallet;
     address public immutable dOrgProjectLogic;
     address public immutable gnosisLogic;
@@ -19,7 +18,7 @@ contract dOrgProjectFactory {
     event GnosisSafeAddress(address _gnosisSafeAddress);
 
     /**
-     * @dev Initializes `dOrgProject` instance. 
+     * @dev Initializes `dOrgProject` instance.
      */
 
     constructor(address _treasuryWallet, address _gnosisLogic) {
@@ -29,22 +28,36 @@ contract dOrgProjectFactory {
     }
 
     /**
-     * @dev 
-     * 1) Creates a `dOrgProject` clone using the `Clones.sol` library. 2) Adds project to project array. 
+     * @dev
+     * 1) Creates a `dOrgProject` clone using the `Clones.sol` library. 2) Adds project to project array.
      * Each account in `payees` is assigned the number of shares at the matching position in the `shares` array.
      * All addresses in `payees` must be non-zero. Both arrays must have the same non-zero length, and there must be no
      * duplicates in `payees`.
      */
 
-    function createProject(string calldata _projectName, address _finderWallet, address[] calldata _owners, uint256 _threshold) external {
-        
+    function createProject(
+        string calldata _projectName,
+        address _finderWallet,
+        address[] calldata _owners,
+        uint256 _threshold
+    ) external {
         address payable gnosisSafe;
         gnosisSafe = payable(Clones.clone(gnosisLogic));
 
-        bytes memory gnosisPayload = abi.encodeWithSignature("setup(address[],uint256,address,bytes,address,address,uint256,address)",_owners, _threshold, 0, address(0), 0, 0, 0);
+        bytes memory gnosisPayload =
+            abi.encodeWithSignature(
+                "setup(address[],uint256,address,bytes,address,address,uint256,address)",
+                _owners,
+                _threshold,
+                0,
+                address(0),
+                0,
+                0,
+                0
+            );
         address(gnosisSafe).call(gnosisPayload);
-        
-        string memory projectName; 
+
+        string memory projectName;
         projectName = _projectName;
 
         address[] memory payees = new address[](3);
@@ -57,14 +70,12 @@ contract dOrgProjectFactory {
         shares[0] = 10;
         shares[1] = 10;
         shares[2] = 80;
-        
+
         address payable project;
         project = payable(Clones.clone(dOrgProjectLogic));
-        dOrgProject(project).initialize(projectName,payees,shares);
+        dOrgProject(project).initialize(projectName, payees, shares);
 
         emit ProjectAddress(project);
         emit GnosisSafeAddress(gnosisSafe);
-        
     }
-
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,24 @@
 const dOrgProjectFactory = artifacts.require("dOrgProjectFactory");
 
 module.exports = function (deployer) {
+
+  console.log("Environment: " + process.env.NODE_ENV)
+
+
+  // Mainnet treasury wallet.
   let treasuryWallet = "0x15344EcDc2c4EDFCB092E284d93c20F0529FD8a6"
-  // Development
-  let gnosisLogic = "0xb4A7C7da1631CF60A2Cf23ABc86986f99a1A7f70"
-  // Rinkeby
-  // let gnosisLogic = "0x6851D6fDFAfD08c0295C392436245E5bc78B0185"
-  deployer.deploy(dOrgProjectFactory, treasuryWallet, gnosisLogic)
+  
+  let gnosisLogicAddress;
+
+  if(process.env.NODE_ENV ==='development'){
+    // Put the address of your locally deployed GnosisSafe.sol here.
+    gnosisLogicAddress = "0xb4A7C7da1631CF60A2Cf23ABc86986f99a1A7f70" 
+  }
+
+  if(process.env.NODE_ENV ==='rinkeby'){
+    // Rinkeby address of GnosisSafe.sol.
+    gnosisLogicAddress = "0xb4A7C7da1631CF60A2Cf23ABc86986f99a1A7f70" 
+  }
+
+  deployer.deploy(dOrgProjectFactory, treasuryWallet, gnosisLogicAddress)
 };
