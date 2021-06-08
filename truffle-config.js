4bec52e18c857a2565063e843001306a54b2c501
Carlos <cfebres>
2021-06-08 19:18:47 -0400

    generate models when react starts/builds

--- truffle-config.js
+++ truffle-config.js
@@ -1,28 +1,9 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
+const dotenv = require("dotenv");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-// const infuraKey = "fj4jll3k.....";
-//
-const fs = require("fs");
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+dotenv.config();
+
+const mnemonic = process.env.NMEMONIC;
 
 module.exports = {
   networks: {
@@ -37,11 +18,8 @@ module.exports = {
           mnemonic,
           `https://rinkeby.infura.io/v3/811d39a9e58e4555b9b91707fa36bb60`
         ),
-      network_id: 4, // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 1, // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+      network_id: 4,
+      confirmations: 1,
     },
   },
   compilers: {
