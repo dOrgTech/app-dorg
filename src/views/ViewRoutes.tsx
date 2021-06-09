b798ec9095b74d6dbc82fc4e73119ab297eddc49
Carlos <cfebres>
2021-06-08 20:16:41 -0400

    clean code

--- src/views/ViewRoutes.tsx
+++ src/views/ViewRoutes.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { ProjectsView } from "./Projects/ProjectsView";
-import { NewProjectView } from "./Projects/NewProjectView";
 
 export const ViewRoutes = () => {
   return (
     <Switch>
-      <Route path="/new" component={NewProjectView} />
       <Route path="/" component={ProjectsView} />
     </Switch>
   );
